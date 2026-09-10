02b11ed1291e4631e929a23d27577326a9dc78ae
Mayank Kumar <103820518+thismayank1@users.noreply.github.com>
2026-09-10 23:28:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ class Animal {
 
 class Dog extends Animal {
 	bark(){
-		console.log("purr")
+		console.log("woof")
 	}
 }
 
 class Cat extends Animal {
 	purr(){
-		console.log("woof")
+		console.log("purr")
 		
 	}
 }
