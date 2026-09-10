f24523ecd1e657e3302d5670fa1f61a81b9d334e
Mayank Kumar <103820518+thismayank1@users.noreply.github.com>
2026-09-10 23:31:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //complete this code
 class Animal {
 	constructor(species){
-		this.species = species;
+		this._species = species;
 	} 
 	get species(){
-		return this.species;
+		return this._species;
 	}
 	makeSound(species){
-		console.log(`The ${species} makes a sound`)
+		console.log(`The ${this.species} makes a sound`)
 		
 	}
 }
